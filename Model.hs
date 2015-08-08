7a4f4965ca011e35719d7e07f9c5f9007fe4ad06
ToF- <cthibaut@octo.com>
2015-08-08 17:14:55 +0200

    revisited design

--- Model.hs
+++ Model.hs
@@ -1,41 +1,35 @@
 module Model
 where
-import Data.List (deleteBy)
+
+type Model = [(Label,Stock)]
+
+data Stock = Stock { 
+    label :: Label,
+    quantity :: Quantity,
+    input    :: Quantity,
+    output   :: Quantity
+    }
+    deriving (Eq, Show)
 
 type Label = String
 type Quantity = Double
-type Model = [(Label,Stock)]
-data Stock = Stock Quantity
+
+addStock :: Stock -> Model -> Model
+addStock s m = (label s, s) : m
 
 initial :: Model
 initial = []
 
 stock :: Model -> Label -> Stock
 stock m l = case lookup l m of
-    Just s -> s
-    Nothing -> error $ "unknown stock:" ++ l 
-    
+    (Just s) -> s
+    Nothing -> error $ "unknown stock: "++l
 
-addStock :: Label -> Quantity -> Model -> Model
-addStock l q m = (l, Stock q) : m
+update :: Label -> Stock -> Model -> Model
+update l s m = addStock s $
+    filter (\(lbl,_) -> lbl /= l) m 
 
-deleteStock :: Label -> Model -> Model
-deleteStock s m = filter (\(l,_) -> l /= s) m
-
-quantity :: Stock -> Quantity
-quantity (Stock q) = q
-
-update :: Label -> (Model -> Quantity) -> Model -> Model
-update l f m = addStock l (f m) (deleteStock l m)
-
-increase :: Label -> (Model -> Quantity) -> Model -> Model
-increase l f m = addStock l (q + f m) (deleteStock l m)
-    where q = quantity (stock m l)
-
-decrease :: Label -> (Model -> Quantity) -> Model -> Model
-decrease l f m = addStock l (q - f m) (deleteStock l m)
-    where q = quantity (stock m l)
-
-
-
-
+runOutput :: (Stock -> Quantity) -> Stock -> Stock
+runOutput f s = s { quantity = q, output = o}
+    where o = f s
+          q = quantity s - o
--- Specs.hs
+++ Specs.hs
@@ -2,32 +2,63 @@ import Test.Hspec
 import Model
 
 main = hspec $ do
+    describe "a Stock" $ do
+        let w = Stock "WATER" 5.2  1.0  0.8
+
+        it "has a label" $ do
+            label w `shouldBe` "WATER"
+
+        it "has a quantity" $ do
+            quantity w `shouldBe` 5.2
+
+        it "has an input" $ do
+            input w `shouldBe` 1.0
+
+        it "has an output" $ do
+            output w `shouldBe` 0.8
+
     describe "a Model" $ do
-        it "is a collection of stocks" $ do
-            let m = addStock "COAL" 3.7 (addStock "WATER" 5.2 initial)
-                s1 = m `stock` "WATER"
-                s2 = m `stock` "COAL"
+        let m = addStock c (addStock w initial)
+            w = Stock "WATER" 5.2 1.0 0.8
+            c = Stock "COAL"  4.9 1.0 1.0
 
-            quantity s1 `shouldBe` 5.2
-            quantity s2 `shouldBe` 3.7
+        it "is a collection of Stocks" $ do
+            length m `shouldBe` 2
 
-        it "can update a stock" $ do
-            let m = update "WATER" updater (addStock "WATER" 5.2 initial)
-                updater = const 4.5
-            quantity (m `stock` "WATER") `shouldBe` 4.5
+        it "can find its stocks by name" $ do
+            stock m "WATER" `shouldBe` w
 
-        it "can increase a stock" $ do
-            let m = increase "WATER" increaser (addStock "WATER" 5.2 initial)
-                increaser = const 1.5
-            quantity (m `stock` "WATER") `shouldBe` 6.7
+        it "can update its stocks" $ do
+            let w' = w { quantity = 3.6 }
+                m' = update "WATER" w' m
+            stock m' "WATER" `shouldBe` w'
+            stock m' "COAL"  `shouldBe` c
 
+    describe "an runOutput function" $ do
+        let w = Stock "WATER" 5.0 1.0 0.8
 
-        it "can decrease a stock" $ do
-            let m = decrease "WATER" decreaser (addStock "WATER" 5.0 initial)
-                decreaser m = (quantity (m `stock` "WATER")) * 0.1
-            quantity (m `stock` "WATER") `shouldBe` 4.5
+        it "updates a stock quantity and output values" $ do
+            let w' = runOutput f w
+                f s = quantity s * 0.1
+            quantity w' `shouldBe` 4.5    
+            output w'   `shouldBe` 0.5
 
-
-
-
+        it "can update a stock output with othe parameters" $ do    
+            let w' = runOutput (f 0.2) w
+                f limit s = min limit (quantity s * 0.1)
+            quantity w' `shouldBe` 4.8
+            output w'   `shouldBe` 0.2
             
+
+
+
+        
+            
+            
+    
+
+        
+        
+
+
+
